7b39ae644fc612c2d23703eb3b93ddf5a720b2b0
daniszd <danielszdatas@gmail.com>
2025-10-23 21:27:42 -0300

    Update RepositorioTarefas.h

--- include/RepositorioTarefas.h
+++ include/RepositorioTarefas.h
@@ -1,1 +1,23 @@
+#ifndef REPOSITORIO_TAREFAS_H
+#define REPOSITORIO_TAREFAS_H
 
+#include "Tarefa.h"
+#include <vector>
+#include <string>
+
+class RepositorioTarefas {
+private:
+    // repositorio sabera em qual arquivo deve salvar
+    std::string Arquivo;
+
+public:
+
+    // Salvar tarefas em arquivo
+    void salvarTarefa(const std::vector<Tarefa>& tarefas);
+
+    // Carrega a lista Rde tarefas do banco de dados ou arquivo
+    std::vector<Tarefa> carregarTarefas();
+
+}
+
+#endif
