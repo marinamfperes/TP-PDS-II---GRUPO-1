6e305c27a351557a3f328b4f84cc7a80f1026242
migueldelcorsi <migueldelcorsi@gmail.com>
2025-10-23 23:18:24 -0300

    Update Usuario.h

--- include/Usuario.h
+++ include/Usuario.h
@@ -7,16 +7,14 @@ class Usuario {
 public:
     explicit Usuario(const std::string& nome);
 
-    // Getters
     std::string getNome() const;
     int getHorasPreferenciaNotificacao() const;
 
-    // Define a antecedência das notificações (em horas)
     void definirHorasPreferenciaNotificacao(int horas);
 
 private:
     std::string nome;
-    int horasPreferenciaNotificacao; // Exemplo: 24, 48 etc.
+    int horasPreferenciaNotificacao; 
 };
 
-#endif // USUARIO_H
+#endif 
