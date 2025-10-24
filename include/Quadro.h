4e73281c801e6ef59405cc60bd49a31f32a78c57
LuisaGouthier <luisagouthier@ufmg.br>
2025-10-23 22:53:22 -0300

    Update Quadro.h

--- include/Quadro.h
+++ include/Quadro.h
@@ -5,17 +5,16 @@
 #include <vector>
 #include <string>
 
-using namespace std;
 
 class Quadro {
   public:
     Quadro();
       void adicionarColuna(const Coluna& coluna); //adicao de colunas
       void exibirQuadro() const; //exibicao das colunas
-      void moverTarefaParaColuna(int id, string novoStatus);
+      void moverTarefaParaColuna(int id,  std::string& novoStatus);
 
   private:
-    vector<Coluna> colunas;
+     std::vector<Coluna> colunas;
 };
 
 #endif
