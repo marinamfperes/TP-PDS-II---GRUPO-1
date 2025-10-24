840a63eec224453bcbc06ce3a5f3668ebfa39008
LuisaGouthier <luisagouthier@ufmg.br>
2025-10-23 22:19:31 -0300

    Create Tarefa.h - arquivo .h da classe Tarefa

---
+++ include/Tarefa.h
@@ -0,0 +1,41 @@
+#ifndef TAREFA_H
+#define TAREFA_H
+
+#include <string>
+#include <vector>
+#include <ctime>  //para manipular data e hora
+
+class Tarefa {
+public:
+    Tarefa(std::string titulo, std::string descricao, std::string categoria,
+           std::time_t agendada, std::time_t vencimento, int prioridade);
+
+    //métodos de acesso
+    std::string getTitulo() const; //acessa o título
+    std::string getDescricao() const; //acessa a descrição
+    std::string getCategoria() const; //acessa a categoria
+    std::time_t getAgendada() const; //acessa a data de realização agendada
+    std::time_t getVencimento() const; //acessa a data de vencimento
+    int getPrioridade() const; //acessa o nível de prioridade
+    std::string getStatus() const; //acessa o status no Kanban
+
+    //métodos de modificação
+    void atualizarStatus(std::string novoStatus); //troca o status no Kanban
+    bool estaVencida(std::time_t agora) const; //verifica se está vencida (overdue)
+    bool estaProximaDoVencimento(std::time_t agora, int janelaHoras) const; //verifica se está próxima do vencimento
+    void addTag(std::string tag); //adiciona tag (categoria)
+    bool validarCampos() const; //verifica se título está preenchido
+    bool validarDatas() const; //verifica se data e horário são válidos
+
+private:
+    std::string titulo;
+    std::string descricao;
+    std::string categoria;
+    std::time_t agendada; //time_t pega o tempo atual do sistema
+    std::time_t vencimento;
+    int prioridade;
+    std::string status;  //"A Fazer", "Fazendo" ou "Feito"
+    std::vector<std::string> tags;  //lista de tags associadas à tarefa
+};
+
+#endif
