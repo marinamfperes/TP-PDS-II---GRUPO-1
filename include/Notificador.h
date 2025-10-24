05193ed65c52906281cee6791a617b791f822f0d
daniszd <danielszdatas@gmail.com>
2025-10-23 21:06:23 -0300

    Update Notificador.h

--- include/Notificador.h
+++ include/Notificador.h
@@ -1,1 +1,28 @@
+#ifndef NOTIFICADOR_H
+#define NOTIFICADOR_H
 
+#include "Tarefa.h"
+#include <vector>
+//sem isso ele abriria o gerenciador, e o arquvo do gerenciador pediria para abrir esse, criando um loop
+class GerenciadorTarefas; // impede uma redundancia ao executar funcoes do gerenciador.h e do notificacao.h
+
+class Notificador {
+private:
+    GerenciadorTarefas& gerenciador; // guarda um atalho para o gerenciador original
+
+    long segundosAntecedencia; // guarda em segundos o tempo de antecedencia (24h = 86400 seg)
+
+    // Verificar tarefas vencidas ou próximas do vencimento
+    void verificarTarefas(std::vector<Tarefa>& tarefas, std::time_t agora);
+
+public:
+    Notificador(GerenciadorTarefas& mgr); // constroi o notificador (usa o GerenciadorTarefas, colaborador)
+
+
+    void setAntecedenciaNotificacao(long segundos); // define periodo considerado prox do vencimento
+
+    // identifica quais vao vencer e sinaliza
+    // retorna a lista de quais nao venceram mass estao dentro da funcao de antecedencia(periodo prox de vencer)
+    std::vector<Tarefa> getTarefasVencidas() const;
+};
+#endif
