743366c082ab19e6c3e049e233399c5be4bbdd4a
Marina Fonseca <marinamfonseca@ufmg.br>
2025-10-29 00:09:10 -0300

    cabeçalho com os includes

--- build/main.cpp
+++ build/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "Coluna.h"
+#include "EstrategiaFiltro.h"
+#include "FiltroCombinado.h"
+#include "FiltroPrioridade.h"
+#include "FiltroTag.h"
+#include "GerenciadorTarefas.h"
+#include "Notificador.h"
+#include "Quadro.h"
+#include "Relogio.h"
+#include "RepositorioTarefas.h"
+#include "Tarefa.h"
+#include "Usuario.h"
 
+using namespace std;
+
+int main() {
+  
+}
