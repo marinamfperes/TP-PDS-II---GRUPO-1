0c1675d1357b03472077922f285f430df494809c
migueldelcorsi <migueldelcorsi@gmail.com>
2025-11-11 20:19:47 -0300

    Gera e atualiza documentação Doxygen

---
+++ html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "1gipt",
+  1: "1gipt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
