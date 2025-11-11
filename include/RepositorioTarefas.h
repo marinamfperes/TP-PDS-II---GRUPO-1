b6c8f0677d8f1cd5a50623d82b25ac2fde414f7c
Marina Fonseca <marinamfonseca@ufmg.br>
2025-11-11 01:11:24 -0300

    Alteração no RepositorioTarefas.h
    Added a backup function to the RepositorioTarefas class.

--- include/RepositorioTarefas.h
+++ include/RepositorioTarefas.h
@@ -1,26 +1,38 @@
 #ifndef REPOSITORIO_TAREFAS_H
 #define REPOSITORIO_TAREFAS_H
 
+
 #include "Tarefa.h"
 #include <vector>
 #include <string>
+
 
 class RepositorioTarefas {
 private:
     // repositorio sabera em qual arquivo deve salvar
     std::string arquivo_;
 
+
 public:
+
 
     // Construtor para definir o arquivo de persistência
     RepositorioTarefas(const std::string& nomeArquivo);
 
+
     // Salvar tarefas em arquivo
     void salvarTarefa(const std::vector<Tarefa>& tarefas) const;
+
 
     // Carrega a lista de tarefas do banco de dados ou arquivo
     std::vector<Tarefa> carregarTarefas() const;
 
+
+    //função para fazer backup do arquivo
+    void arquivoBackup() const;
+
+
 };
+
 
 #endif
